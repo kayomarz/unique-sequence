2d18580fcefe4875a4cfe0aec61acfc3ef085603
moku <moku@pine>
2021-12-19 15:58:22 +0530

    README, examples

--- examples/01-unique-sequences.js
+++ examples/01-unique-sequences.js
@@ -1,69 +1,6 @@
-# unique-sequence
+// To run this example:
+// node 01-unique-sequence.js
 
-Generate a sequence of short strings unique within their scope.
-
-This utility is built using [Node.js](https://nodejs.org/en/about/).
-
-```js
-const { generatorCustom } = require('unique-sequence');
-
-// generate a sequence using elements 'a', 'b'
-const gen = generatorCustom(['a', 'b']);
-console.log(gen.next().value); // a
-console.log(gen.next().value); // b
-console.log(gen.next().value); // ba
-console.log(gen.next().value); // bb
-```
-
-# Generators available
-
-1. generatorCustom
-2. generatorAlphaUpper
-3. generatorAlphaLower
-4. generatorAlphaNumUpper
-5. generatorAlphaNumLower
-6. generatorNum
-
-# Introduction
-
-This library generates unique short strings. The generator starts out by
-creating strings of just one character. 
-
-A list of strings are used to generate a sequence of strings similar to the
-sequence of natural numbers or integers.
-
-For instance, below is a sequence of natural numbers generated using the list of
-decimal digits `0` to `9`.
-
-```
-0,   1,  2,  3,  4,  5,  6,  7,  8,  9,
-10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-30
-```
-
-Similarly, below is a sequence using the list of latin alphabets `a` to `j`
-
-```
- a,  b,  c,  d,  e,  f,  g,  h,  i,  j,
-ba, bb, bc, bd, be, bf, bg, bh, bi, bj,
-ca, cb, cc, cd, ce, cf, cg, ch, ci, cj,
-da
-```
-
-In a similar manner, this library generates sequences, given a list of strings.
-
-# Getting started
-
-## Install this library
-
-```bash
-npm install unique-sequence
-```
-
-## Example program
-
-```js
 const {
   generatorCustom,
   generatorNum,
@@ -73,11 +10,8 @@ const {
   generatorAlphaNumLower,
 } = require('unique-sequence');
 
-/* Lets generate a sequence using two elements 'a' and 'b' */
-const list = ['a', 'b'];
-
-// NOTE: list length must be 2 to 36.
-
+/* Let's generate a sequence using two elements 'a' and 'b' */
+const list = ['a', 'b']; // NOTE: length must be 2 to 36.
 const gen2 = generatorCustom(list);
 console.log(gen2.next().value); // a
 console.log(gen2.next().value); // b
@@ -89,7 +23,7 @@ console.log(gen2.next().value); // bba
 console.log(gen2.next().value); // bbb
 console.log(gen2.next().value); // baaa
 
-/* Using three elements 'x', 'y', 'z' */
+/* Let's generate sequence using two elements 'a' and 'b' */
 const gen3 = generatorCustom(['x', 'y', 'z']);
 console.log(gen3.next().value); // x
 console.log(gen3.next().value); // y
@@ -102,7 +36,7 @@ console.log(gen3.next().value); // zy
 console.log(gen3.next().value); // zz
 console.log(gen3.next().value); // yxx
 
-/* Let us make a helper to iterate over the sequence generator  */
+/* Let's make a helper to iterate over the sequence generator  */
 function times(gen, count = 64) {
   const arr = [];
   for (let i = 0; i < count; i++) {
@@ -111,7 +45,7 @@ function times(gen, count = 64) {
   return arr;
 }
 
-// Generate 10 strings, just like our previous example.
+// Generate 10 sequences, just like our previous example.
 console.log(times(generatorCustom(['x', 'y', 'z']), 10));
 // [
 //   'x',   'y',  'z',
@@ -120,7 +54,7 @@ console.log(times(generatorCustom(['x', 'y', 'z']), 10));
 //   'yxx'
 // ]
 
-/* Lets generate 64 stings using upercase latin alphabets */
+/* Let's generate 64 sequences using upercase latin alphabets */
 console.log(times(generatorAlphaUpper())); // 64 is the default
 // [
 //   'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H',
@@ -133,7 +67,8 @@ console.log(times(generatorAlphaUpper())); // 64 is the default
 //   'CE', 'CF', 'CG', 'CH', 'CI', 'CJ', 'CK', 'CL'
 // ]
 
-/* There's lower case too */
+/* We can generate lower case too */
+
 console.log(times(generatorAlphaLower()));
 // [
 //   'a',  'b',  'c',  'd',  'e',  'f',  'g',  'h',
@@ -146,7 +81,7 @@ console.log(times(generatorAlphaLower()));
 //   'ce', 'cf', 'cg', 'ch', 'ci', 'cj', 'ck', 'cl'
 // ]
 
-/* Alpha Numeric too */
+/* There is Alpha Numeric too */
 console.log(times(generatorAlphaNumLower()));
 // [
 //   '0',  '1',  '2',  '3',  '4',  '5',  '6',  '7',
@@ -172,7 +107,7 @@ console.log(times(generatorAlphaNumUpper()));
 //   '1K', '1L', '1M', '1N', '1O', '1P', '1Q', '1R'
 // ]
 
-/* For the sake of completion, use decimal digits */
+/* For the sake of completion too, we can use only numbers */
 console.log(times(generatorNum()));
 // [
 //   '0',  '1',  '2',  '3',  '4',  '5',  '6',  '7',
@@ -212,14 +147,13 @@ console.log(times(generatorCustom(['rat,', 'a', 'tat'])));
 //   'tatarat,rat,'
 // ]
 
-/* Lower case alphabets again, but this time specify the characters. */
+/* Again, lets generate using all latin alphabets */
 console.log(times(generatorCustom([
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
   'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
   'y', 'z',
 ])));
-
 // [
 //   'a',  'b',  'c',  'd',  'e',  'f',  'g',  'h',
 //   'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',
@@ -247,18 +181,3 @@ console.log(times(generatorCustom([
   console.error('error:', e.message);
 }
 // error: maximum list length is 36, got 52.
-
-// Thats because length must be 2 to 36.
-```
-
-
-# Change log
-
-## version 1.0.0
-
-2021-12-16
-
-+ Initial code to generate custom sequence, upper/lowercase, alphanum.
-
-
-Note: Change log dates are yyyy-mm-dd.
